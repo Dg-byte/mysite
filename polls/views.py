30a8ca47feb5ca401db7f70bc2ebd481d76a8ae2
Dg-byte <dgolikov40@gmail.com>
2021-02-22 19:52:30 +0700

    My message

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello world</h1>")
+
+def meme(request):
+    return HttpResponse("https://upload.wikimedia.org/wikipedia/ru/4/4d/Wojak.png")
