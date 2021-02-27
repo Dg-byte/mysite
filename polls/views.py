53ad71313fbce33094cd250203ce270f1904c453
Dg-byte <dgolikov40@gmail.com>
2021-02-27 19:53:29 +0700

    my message

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question, Choice 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello world</h1>")
+    q_all = Question.objects.all()
+    res = "<ol>"
+    for q in q_all:
+        res += "<li>%s</li>" % q.text
+    res += "</ol>"
+
+    return HttpResponse(res)
+
+
+def detail(request, q_id):
+    res = "Question number %s." % q_id
+    return HttpResponse(res)
+
+def results(request, q_id):
+    res = "Result for questions number %s." % q_id
+    return HttpResponse(res)   
+
+def votes(request, q_id):
+    res = "Vote for questions number %s." % q_id
+    return HttpResponse(res)    
 
 def meme(request):
-    return HttpResponse('<img src = https://upload.wikimedia.org/wikipedia/ru/4/4d/Wojak.png></img>')
+    return HttpResponse('<img src = https://upload.wikimedia.org/wikipedia/ru/4/4d/Wojak.png    ></img>')
